26d6ac67ced11e80932cb7c3182a85a81757bec4
Xinjn <422075616@qq.com>
2021-08-07 16:57:11 +0800

    first commit

--- src/pages/My/template.vue
+++ src/pages/My/template.vue
@@ -4,9 +4,12 @@
     <section class="user-info">
       <img :src="user.avatar" alt="" class="avatar">
       <h3>{{user.username}}</h3>
+
     </section>
 
-    <section v-for="blog in blogs" :key="blog.createdAt">
+    <div v-if="blogs.length === 0" class="tip">暂无文章...</div>
+
+    <section v-for="blog in blogs" :key="blog.createdAt" >
       <router-link class="item" :to="`/edit/${blog.id}`">
         <div class="date">
           <span class="day">{{splitDate(blog.createdAt).date}}</span>
@@ -14,7 +17,7 @@
           <span class="year">{{splitDate(blog.createdAt).year}}</span>
         </div>
         <h3>{{blog.title}}</h3>
-        <p>{{blog.description}}</p>
+        <p >{{blog.description}}</p>
         <div class="actions">
             <el-button href="#" @click.prevent="onDelete(blog.id)">删除</el-button>
             <router-Link :to="`/edit/${blog.id}`"><el-button>编辑</el-button></router-Link>
